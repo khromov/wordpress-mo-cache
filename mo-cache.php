54f169bd3b2078d32c0fbac6864e0c6ec20d37c9
khromov <stanislav.khromov@gmail.com>
2013-08-04 04:18:02 +0200

    Commented code, changed to MD5 for key generation

--- mo-cache.php
+++ mo-cache.php
@@ -8,49 +8,74 @@ Plugin Name: MO Cache
 Plugin URI: http://wordpress.org/extend/plugins/mo-cache/
 Description: Improving the site performance by caching translation files using the WordPress standard cache mechanism.
 Author: Masaki Takeuchi
-Version: 2.0
+Version: 12.1
 Author URI: https://github.com/m4i/wordpress-mo-cache
 License: MIT
 */
 
-class MOCache {
-	const VERSION = '2.0';
+class MOCache
+{
+	const VERSION = '12.1';
 	const GROUP   = 'mo';
 
-	public static function setup() {
-		add_filter(
-			'override_load_textdomain', array( new self, 'load' ), 10, 3 );
+	public static function setup()
+	{
+		//Override loading of mo files
+		add_filter('override_load_textdomain', array( new self, 'load' ), 10, 3 );
 	}
-
-	public function load( $override, $domain, $mofile ) {
+	
+	/** This will substitute load_textdomain, which usually resides in wp-includes/l10n.php **/
+	public function load($override, $domain, $mofile)
+	{
 		global $l10n;
+		
+		//Perform actions which are hooked to load_textdomain
+		do_action('load_textdomain', $domain, $mofile);
+		
+		//Apply filters for plugins that modify .mo file paths (adding, deleting them etc)
+		$mofile = apply_filters('load_textdomain_mofile', $mofile, $domain );
+		
+		//Check if the translation file exists.
+		if (!is_readable($mofile))
+			return false;
+		
+		//Add a cache global group. This relies on object-cache.php, whichever you have.
+		if(function_exists('wp_cache_add_global_groups'))
+			wp_cache_add_global_groups(self::GROUP);
 
-		do_action( 'load_textdomain', $domain, $mofile );
-		$mofile = apply_filters( 'load_textdomain_mofile', $mofile, $domain );
-		if ( !is_readable( $mofile ) ) return false;
-
-		if ( function_exists( 'wp_cache_add_global_groups' ) ) {
-			wp_cache_add_global_groups( self::GROUP );
-		}
-		$key = preg_replace('/[^-\w]/', '_',
-			self::VERSION . "-${GLOBALS['wp_version']}-$mofile");
-
+		//Generate object cache key name
+		$key = md5(self::VERSION . "-${GLOBALS['wp_version']}-$mofile");
+		
 		$mo = new MO();
-		if ( $cache = wp_cache_get( $key, self::GROUP ) ) {
+		
+		//Do we have cache?
+		if ($cache = wp_cache_get($key, self::GROUP))
+		{
+			//Yes! Set the entries
 			$mo->entries = $cache['entries'];
 			$mo->set_headers( $cache['headers'] );
-		} else {
-			if ( !$mo->import_from_file( $mofile ) ) return false;
+		}
+		else //No cache, let's prime it
+		{
+			//Attempt to load from file
+			if (!$mo->import_from_file($mofile))
+				return false;
+			
+			//If we managed to load the file, set the entries
 			$cache = array(
 				'entries' => $mo->entries,
 				'headers' => $mo->headers,
 			);
-			wp_cache_set( $key, $cache, self::GROUP );
+			
+			//Put in cache! //serialize()
+			wp_cache_set($key, $cache, self::GROUP);
 		}
 
-		if ( isset( $l10n[$domain] ) )
-			$mo->merge_with( $l10n[$domain] );
+		if (isset($l10n[$domain]))
+			$mo->merge_with($l10n[$domain]);
+		
 		$l10n[$domain] = &$mo;
+		
 		return true;
 	}
 }
